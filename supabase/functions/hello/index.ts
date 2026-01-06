0d7bcbbf12b59e334b7adb92df2252fb9995c230
NMMonteiro <nunommonteiro@msn.com>
2026-01-06 12:31:24 +0000

    debug: add import to hello function to test dependency resolution

--- supabase/functions/hello/index.ts
+++ supabase/functions/hello/index.ts
@@ -1,3 +1,5 @@
+import { createClient } from 'npm:@supabase/supabase-js@2';
+
 Deno.serve(async (req) => {
     return new Response(JSON.stringify({ message: "Hello from Brand New Function" }), {
         headers: { "Content-Type": "application/json" }
